b75fd656677f8f1e7611a47585182fe0bf5e5166
abdiaschafy <abdiaschafang@gmail.com>
2018-02-28 08:13:17 +0100

    create and static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laracarte</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <title>Laracarte</title>
+</head>
+<body class="container" style="padding-top:60px ">
+	@include('layouts.partials._nav')
+	@yield('contenu')
+	@include('layouts.partials._footer')
+</body>
+
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<style>
+	body{
+		font-family: 'Open Sans', sans serif;
+	}
+	footer{
+		margin: 4em 0;
+	}
+</style>
+
+</html>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+	<p>&copy; {{date('Y')}} · Laracarte by <a href="http://twitter.com/chafy07">@chafy07</a> - A Laramap clone app for TDN.</p>
+
+	<p><b>This app has been built for learning purposes.</b></p>
+</footer>
